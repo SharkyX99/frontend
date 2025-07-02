98aa32626e1a7742990c571b0bde406a2a0f9c34
NANGpisitpong <pisitpong04@gmail.com>
2025-07-02 09:06:27 +0700

    1

--- app/service/page.js
+++ app/service/page.js
@@ -1,23 +1,27 @@
-import Navigation from '../components/navigation.js';
+import Navigation from "../components/navigation.js";
 
 export default function AboutPage() {
   return (
     <>
       <Navigation />
-      <main style={{
-  paddingTop: '80px', // ระยะเท่ากับ Navbar
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'center',
-  alignItems: 'center',
-  height: '80vh',
-  textAlign: 'center',
-  padding: '2rem'
-}}> …
-        <h1>เกี่ยวกับ Nuttanawat Manakit</h1>
-        <p>สวัสดีครับ ผมชื่อ Nuttanawat หรือ Shar นักพัฒนาเว็บด้วย Next.js...</p>
+      <main
+        style={{
+          paddingTop: "80px", // ระยะเท่ากับ Navbar
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "80vh",
+          textAlign: "center",
+          padding: "2rem",
+        }}
+      >
+        {" "}
+        …<h1>เกี่ยวกับ Nuttanawat Manakit</h1>
+        <p>
+          สวัสดีครับ ผมชื่อ Nuttanawat หรือ Shar นักพัฒนาเว็บด้วย Next.js...
+        </p>
       </main>
     </>
   );
 }
-
