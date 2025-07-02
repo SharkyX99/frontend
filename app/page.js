50bbe1589862a13af3534a505f31a2ae75c4e686
NANGpisitpong <pisitpong04@gmail.com>
2025-07-02 11:40:38 +0700

    11

--- app/page.js
+++ app/page.js
@@ -66,22 +66,6 @@ export default function Home() {
 
           {/* Card Section (Cars) */}
           <Card />
-
-          {/* Additional Info Section */}
-          <div className="row mt-5">
-            <div className="col-12">
-              <div className="card bg-light">
-                <div className="card-body text-center">
-                  <h3 className="card-title">พร้อมที่จะเริ่มต้นโปรเจคใหม่?</h3>
-                  <p className="card-text">
-                    ไม่ว่าคุณจะต้องการพัฒนาเว็บไซต์ใหม่ หรือปรับปรุงเว็บไซต์เดิม เราพร้อมช่วยเหลือคุณ
-                  </p>
-                  <a href="/contact" className="btn btn-success btn-lg">เริ่มต้นเลย</a>
-                </div>
-              </div>
-            </div>
-          </div>
-
         </div>
       </div>
     </div>
